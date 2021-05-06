03263363d17d34d80c0e78b5d090461d751b4db0
m081779 <marcoprincipio@hotmail.com>
2021-05-06 00:16:55 -0400

    adding units static file

---
+++ assets/units.ts
@@ -0,0 +1,9 @@
+const units = [
+  { name: 'Ounces', value: 'ounces' },
+  { name: 'Cups', value: 'cups' },
+  { name: 'Pounds', value: 'pounds' },
+  { name: 'Grams', value: 'grams' },
+  { name: 'Millileter', value: 'milliliter' }
+]
+
+export default units
