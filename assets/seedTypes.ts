0afb6c01fc89459cfaf3648b9bee1fae304a4c71
m081779 <marcoprincipio@hotmail.com>
2021-05-11 18:08:21 -0400

    Added seedTypes

--- assets/seedTypes.ts
+++ assets/seedTypes.ts
@@ -3,7 +3,22 @@ const seedTypes = [
   { name: 'Pea', value: 'pea' },
   { name: 'Radish', value: 'radish' },
   { name: 'Wheat Grass', value: 'wheatgrass' },
-  { name: 'Broccoli', value: 'broccoli' }
+  { name: 'Broccoli', value: 'broccoli' },
+  { name: 'Mustard', value: 'mustard' },
+  { name: 'Basil', value: 'basil' },
+  { name: 'Onion', value: 'onion' },
+  { name: 'Leek', value: 'leek' },
+  { name: 'Chives', value: 'chives' },
+  { name: 'Kale', value: 'kale' },
+  { name: 'Cabbage', value: 'cabbage' },
+  { name: 'Lettuce', value: 'lettuce' },
+  { name: 'Arugula', value: 'arugula' },
+  { name: 'Swiss Chard', value: 'swiss chard' },
+  { name: 'Beet', value: 'beet' },
+  { name: 'Dill', value: 'dill' },
+  { name: 'Cilantro', value: 'cilantro' },
+  { name: 'Carrot', value: 'carrot' },
+  { name: 'Buckwheatd', value: 'buckwheat' },
 ]
 
 export default seedTypes
